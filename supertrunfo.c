1a710a8c7051631341ac4653320459dfb72df118
Italorchha00 <italorochha00@gmail.com>
2025-09-05 14:13:21 -0300

    Add files via upload

---
+++ supertrunfo.c
@@ -0,0 +1,67 @@
+#include <stdio.h> 
+int main() {
+    char estado1[10];
+    int carta1;
+    char cidade1[30];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+    char estado2[10];
+    int carta2;
+    char cidade2[30];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+    printf("Estado: ");
+    scanf("%s", estado1);
+    printf("Codigo da Carta: ");
+    scanf("%d", &carta1);
+    printf("Cidade: ");
+    scanf("%s", cidade1);
+    printf("Populacao: ");
+    scanf("%d", &populacao1);
+    printf("Area: ");
+    scanf("%f", &area1);
+    printf("PIB: ");
+    scanf("%f", &pib1);
+    printf("Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos1);
+    printf("\n");
+    printf("CARTA 1\n");
+    printf("Estado: %s\n", estado1);
+    printf("Codigo da Carta: %d\n", carta1);
+    printf("Cidade: %s\n", cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Pontos Turisticos: %d\n", pontosTuristicos1);
+    printf("\n");
+    printf("Estado: ");
+    scanf("%s", estado2);
+    printf("Codigo da Carta: ");
+    scanf("%d", &carta2);
+    printf("Cidade: ");
+    scanf("%s", cidade2);
+    printf("Populacao: ");
+    scanf("%d", &populacao2);
+    printf("Area: ");
+    scanf("%f", &area2);
+    printf("PIB: ");
+    scanf("%f", &pib2);
+    printf("Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos2); 
+    printf("\n");
+    printf("CARTA 2\n");
+    printf("Estado: %s\n", estado2);
+    printf("Codigo da Carta: %d\n", carta2);
+    printf("Cidade: %s\n", cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontosTuristicos2);
+   
+    
+    return 0;
+}
